0ca27a862328621cdf2cc9c57f66071c613a7bb5
idktrolled <camilocarlos1100@gmail.com>
2026-03-13 15:46:59 -0500

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,7 +1,6 @@
 package;
 
-#if !android
-#if windows
+/*#if windows
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
 
@@ -87,5 +86,4 @@ class DiscordClient
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
 }
-#end
-#end
+#end*/
