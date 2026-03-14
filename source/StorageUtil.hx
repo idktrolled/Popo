2d57124c79a24a677a7ed8d9fb38021d7403ba99
idktrolled <camilocarlos1100@gmail.com>
2026-03-14 13:57:39 -0500

    Update StorageUtil.hx

--- source/StorageUtil.hx
+++ source/StorageUtil.hx
@@ -7,6 +7,17 @@ import sys.io.*;
 import lime.system.System as LimeSystem;
 import haxe.io.Path;
 import haxe.Exception;
+#if android
+ import android.content.Context as AndroidContext;
+ import android.widget.Toast as AndroidToast;
+ import android.os.Environment as AndroidEnvironment;
+ import android.Permissions as AndroidPermissions;
+ import android.Settings as AndroidSettings;
+ import android.Tools as AndroidTools;
+ import android.os.Build.VERSION as AndroidVersion;
+ import android.os.Build.VERSION_CODES as AndroidVersionCode;
+ import android.os.BatteryManager as AndroidBatteryManager;
+ #end
 
 using StringTools;
 
