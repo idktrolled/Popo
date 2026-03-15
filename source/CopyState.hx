eee8227599ca8a21f5c1b3d27a04396eaee67200
idktrolled <camilocarlos1100@gmail.com>
2026-03-15 16:54:55 -0500

    Update CopyState.hx

--- source/CopyState.hx
+++ source/CopyState.hx
@@ -47,8 +47,6 @@ import android.os.Build.VERSION_CODES as AndroidVersionCode;
 import android.os.BatteryManager as AndroidBatteryManager;
 #end
 
-import StringTools;
-
 class CopyState extends MusicBeatState
 {
 	public static var locatedFiles:Array<String> = [];
