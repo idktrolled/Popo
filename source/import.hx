27adc2371c31599f569c0438407c566fe5aa5b8a
idktrolled <camilocarlos1100@gmail.com>
2026-03-16 20:34:38 -0500

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -13,7 +13,7 @@ import android.os.BatteryManager as AndroidBatteryManager;
 #end
 
 //Flixel
-import flixel.sound.FlxSound;
+import flixel.system.FlxSound;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
