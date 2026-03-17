64c67404e568acc7f402a30d80a6b5fd1cde3960
idktrolled <camilocarlos1100@gmail.com>
2026-03-16 20:32:15 -0500

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,1 +1,40 @@
 import Paths;
+
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
+//Flixel
+import flixel.sound.FlxSound;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.util.FlxDestroyUtil;
+#if sys
+import sys.*;
+import sys.io.*;
+#elseif js
+import js.html.*;
+#end
+#if LUA_ALLOWED
+import llua.*;
+import llua.Lua;
+#end
+using StringTools;
