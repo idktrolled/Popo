470f1f19dba5f2c2db00f5a56f19fbc6c8b2f63a
idktrolled <camilocarlos1100@gmail.com>
2026-03-18 21:24:53 -0500

    Replace MusicBeatState with FlxG for state switching

--- source/CopyState.hx
+++ source/CopyState.hx
@@ -42,7 +42,7 @@ class CopyState extends MusicBeatState
 		checkExistingFiles();
 		if (maxLoopTimes <= 0)
 		{
-			MusicBeatState.switchState(new Init());
+			FlxG.switchState(new Init());
 			return;
 		}
 
